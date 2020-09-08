bdb0ded4a90dc9f937cf5eb2c4d6884d6f234280
francepants <franceline90@gmail.com>
2020-09-08 17:14:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+//Argument(s) - A 4-element Array of a String, String, String, and Number corresponding to a first name, family name, title, and pay rate per hour
+function createEmployeeRecord(array) {
+    let employee = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employee
+}
+
+////
+function createEmployeeRecords(array) {
+    const emp = array.map(x => createEmployeeRecord(x))
+    return emp
+}
